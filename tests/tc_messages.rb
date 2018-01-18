d3746c3805fe1c160b71be1598160a06d90c99c4
Dmitriy Kozlov <you.are.empty@gmail.com>
2018-01-18 23:26:24 +0300

    Small changes in desriptions of tests

--- tests/tc_messages.rb
+++ tests/tc_messages.rb
@@ -8,42 +8,42 @@ require_relative '../vk.rb'
 
 class TestMessages < Test::Unit::TestCase
   def test_start
-    assert_equal(true, Messages.start.is_a?(String), "Method start doesn't return String.")
+    assert_equal(true, Messages.start.is_a?(String), 'Method start should always return String.')
   end
 
   def test_help
-    assert_equal(true, Messages.help.is_a?(String), "Method help doesn't return String.")
+    assert_equal(true, Messages.help.is_a?(String), 'Method help should always return String.')
   end
 
   def test_list
-    assert_equal(true, Messages.list.is_a?(String), "Method list doesn't return String.")
+    assert_equal(true, Messages.list.is_a?(String), 'Method list should always return String.')
   end
 
   def test_random
-    assert_equal(true, Messages.random.is_a?(String), "Method random doesn't work without parameter.")
+    assert_equal(true, Messages.random.is_a?(String), 'Method random should always return String even without parameter.')
 
     ['fkgjdf;kgj;fdkgjf;kg;f', 349_758, 45_984_958.0, true, false, nil, Object].each do |x|
-      assert_equal(true, Messages.random(x).is_a?(String), "Method random doesn't work with wrong album name as #{x.class}.")
+      assert_equal(true, Messages.random(x).is_a?(String), 'Method random should always return String even with wrong album name as #{x.class}.')
     end
 
-    assert_equal(true, Messages.random('Easy').is_a?(String), "Method random doesn't work with correct album name.")
+    assert_equal(true, Messages.random('Easy').is_a?(String), 'Method random should always return String with correct album name.')
   end
 
   def test_all
-    assert_equal(true, Messages.all.is_a?(String), "Method all doesn't work without parameter.")
+    assert_equal(true, Messages.all.is_a?(String), 'Method all should always return String even without parameter.')
 
     ['fkgjdf;kgj;fdkgjf;kg;f', 349758, 45_984_958.0, true, false, nil, Object].each do |x|
-      assert_equal(true, Messages.all(x).is_a?(String), "Method all doesn't work with wrong album name as #{x.class}.")
+      assert_equal(true, Messages.all(x).is_a?(String), 'Method all should always return String even with wrong album name as #{x.class}.')
     end
 
-    assert_equal(true, Messages.all('Easy').is_a?(String), "Method all doesn't work with correct album name.")
+    assert_equal(true, Messages.all('Easy').is_a?(String), 'Method all should always return String with correct album name.')
   end
 
   def test_source
-    assert_equal(true, Messages.source.is_a?(String), "Method source doesn't return String.")
+    assert_equal(true, Messages.source.is_a?(String), 'Method source should always return String.')
   end
 
   def test_other
-    assert_equal(true, Messages.other.is_a?(String), "Method other doesn't return String.")
+    assert_equal(true, Messages.other.is_a?(String), 'Method other should always return String.')
   end
 end
