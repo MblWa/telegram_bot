c2318d9ac2e738d861828ac6062e5c9fe4532b5c
Dmitriy Kozlov <you.are.empty@gmail.com>
2018-01-18 23:29:49 +0300

    Changes in tests descrip. for Vk class

--- tests/tc_vk.rb
+++ tests/tc_vk.rb
@@ -7,22 +7,22 @@ require 'json'
 
 class TestVk < Test::Unit::TestCase
   def test_albums
-    assert_equal(true, Vk.albums_get.is_a?(Hash), "Method albums_get doesn't return Hash.")
+    assert_equal(true, Vk.albums_get.is_a?(Hash), 'Method albums_get should return Hash.')
   end
 
   def test_photos
-    assert_equal(true, Vk.photos_get.is_a?(String), "Method photos_get doesn't work without parameters.")
-    assert_equal(true, Vk.photos_get('Easy').is_a?(String), "Method photos_get doesn't work with correct album name.")
-    assert_equal(true, Vk.photos_get('f;klgjf').is_a?(String), "Method photos_get doesn't work with wrong album name.")
+    assert_equal(true, Vk.photos_get.is_a?(String), 'Method photos_get should return String even without parameters.')
+    assert_equal(true, Vk.photos_get('Easy').is_a?(String), 'Method photos_get should return String with correct album name.')
+    assert_equal(true, Vk.photos_get('f;klgjf').is_a?(String), 'Method photos_get should return String with wrong album name.')
   end
 
   def test_all
-    assert_equal(true, Vk.all_get.is_a?(String), "Method all_get doesn't work without parameters.")
-    assert_equal(true, Vk.all_get('Easy').is_a?(String), "Method all_get doesn't work with correct album name.")
-    assert_equal(true, Vk.all_get('f;klgjf').is_a?(String), "Method all_get doesn't work with wrong album name.")
+    assert_equal(true, Vk.all_get.is_a?(String), 'Method all_get should return String without parameters.')
+    assert_equal(true, Vk.all_get('Easy').is_a?(String), 'Method all_get should return String with correct album name.')
+    assert_equal(true, Vk.all_get('f;klgjf').is_a?(String), 'Method all_get should return String with wrong album name.')
   end
 
   def test_source
-    assert_equal(true, Vk.source_get.is_a?(String), "Method source_get doesn't return String.")
+    assert_equal(true, Vk.source_get.is_a?(String), 'Method source_get should return String.')
   end
 end
